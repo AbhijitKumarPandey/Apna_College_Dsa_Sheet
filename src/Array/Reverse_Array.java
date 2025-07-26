62145cd44cff3560dd5d73f490e7703454b38dc2
Abhijit Kumar Pandey <abhijitkumar.fakila667@gmail.com>
2025-07-26 23:41:49 +0530

    Reverse_Array

--- src/Array/Reverse_Array.java
+++ src/Array/Reverse_Array.java
@@ -1,5 +1,33 @@
 package Array;
 
+import java.security.DomainCombiner;
+
 public class Reverse_Array {
+	public static void ReverseArray(int []arr) {
+		int n = arr.length;
+		
+		int [] temp = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			temp[i] = arr[n-i-1];
+		}
+			
+			for(int i=0; i<n; i++) 
+				arr[i]=temp[i];
+				
+			
+		}
+			
+		public static void main(String args[]) {
+			int arr[] = {1,4,3,2,6,5};
+			ReverseArray(arr);
+			for(int i=0; i<arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			
+		}
+				
+		}
+		
+	
 
 }
